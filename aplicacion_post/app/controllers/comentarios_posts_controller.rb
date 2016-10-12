f699256da8c8dabbbba3f322503f7eaece52a4fc
jhon jairo <jjms95@hotmail.com>
2016-10-11 23:27:19 -0500

    agregando el controlador y vista para crear un comentario completado,y la prueba tambien se completo

--- aplicacion_post/app/controllers/comentarios_posts_controller.rb
+++ aplicacion_post/app/controllers/comentarios_posts_controller.rb
@@ -7,4 +7,21 @@ class ComentariosPostsController < ApplicationController
 		@post = Post.find(params[:post_id])
 		@comentarios_post = @post.comentarios_posts.new
 	end
+
+	def create
+		@post = Post.find(params[:post_id])
+		@comentarios_post = @post.comentarios_posts.new(comentarios_post_param)
+		if @comentarios_post.save
+			flash[:success] = "El comentario se agrego."
+			redirect_to post_comentarios_posts_path
+		else
+			flash[:error] = "hubo un error al agregar el comentario."
+			render action: :new
+		end
+	end
+
+	private
+	def comentarios_post_param 
+		params[:comentarios_post].permit(:contenido)
+	end
 end
